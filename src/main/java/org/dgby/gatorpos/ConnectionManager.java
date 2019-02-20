cb19baac97a66239b3b1f42d469e7b9cd80dca94
Ryan Hazewski <Rhazewski@gmail.com>
2019-02-20 02:06:46 -0500

    Changed pom.hml from main to app

--- src/main/java/org/dgby/gatorpos/ConnectionManager.java
+++ src/main/java/org/dgby/gatorpos/ConnectionManager.java
@@ -5,17 +5,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionManager {
-    private static String url = "jdbc:sqlite::memory";
+    private static String url = "jdbc:sqlite::memory:";
     private static Connection con = null;
 
     public static Connection getConnection() {
         try {
             con = DriverManager.getConnection(url);
+            System.out.println("connection worked");
         } catch (SQLException ex) {
             System.out.println("Failed to connect to database.");
             con = null;
         }
-
+    
         return con;
     }
 }
