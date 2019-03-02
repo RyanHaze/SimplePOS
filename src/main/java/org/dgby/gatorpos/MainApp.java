7476c5cfaa0a23c8e9cec7e7df7bf5fdb249aa03
Ryan Hazewski <Rhazewski@gmail.com>
2019-03-02 06:43:44 -0500

    virtual keyboard works
    vk works

--- src/main/java/org/dgby/gatorpos/MainApp.java
+++ src/main/java/org/dgby/gatorpos/MainApp.java
@@ -31,6 +31,10 @@ public class MainApp extends Application {
         }
 
         public static void main(String[] args) {
+
+                System.setProperty("com.sun.javafx.isEmbedded", "true");
+                System.setProperty("com.sun.javafx.touch", "true");
+                System.setProperty("com.sun.javafx.virtualKeyboard", "javafx");
                 launch(args);
         }
 }
