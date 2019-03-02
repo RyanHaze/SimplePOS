e1ccc0b1698ae4f4d1bcaa86ac071ba087e0efa1
Ryan Hazewski <Rhazewski@gmail.com>
2019-03-02 05:44:18 -0500

    virtual keyboard test
    testing virtual keyboard

--- src/main/java/org/dgby/gatorpos/controllers/PaymentScreenController.java
+++ src/main/java/org/dgby/gatorpos/controllers/PaymentScreenController.java
@@ -1,14 +1,22 @@
 package org.dgby.gatorpos.controllers;
 
+import com.sun.javafx.scene.control.skin.FXVK;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
 import org.dgby.gatorpos.SceneManager;
 
 import java.io.IOException;
 
 public class PaymentScreenController {
 
+    @FXML private TextField cashTendered_tf;
+
+    public void initialize()
+    {
+        cashTendered_tf.getProperties().put(FXVK.VK_TYPE_PROP_KEY, "numeric");
+    }
 
     // Employee button method
     public void backToCurrentTab(ActionEvent event) throws IOException {
@@ -16,6 +24,8 @@ public class PaymentScreenController {
 
 
     }
+
+
 
 
 }
