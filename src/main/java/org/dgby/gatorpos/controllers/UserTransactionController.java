ec2abf52f3ac70f0de25862517d615939384b91c
Ryan Hazewski <Rhazewski@gmail.com>
2019-03-20 01:02:01 -0400

    Small changes to user transactio UI
    enable and disable

--- src/main/java/org/dgby/gatorpos/controllers/UserTransactionController.java
+++ src/main/java/org/dgby/gatorpos/controllers/UserTransactionController.java
@@ -22,13 +22,28 @@ public class UserTransactionController {
     private List<Tab> tabList = new ArrayList<>();
     private Map<Tab, List<Button>> buttonMap = new HashMap<>();
 
+
     @FXML
     private TabPane tabPane;
 
-    @FXML
-    private Button ready_Button, done_Button, priceChange_Button, void_Button, reorder_Button, newcc_Button,
-            storedcc_Button, exactCash_Button, tender_Button;
 
+
+
+    // Function buttons
+    @FXML
+    private Button ready_Button, done_Button, void_Button, reorder_Button, newcc_Button,
+            storedcc_Button, exactCash_Button, tender_Button, tenFC_Button, twentyFC_Button,
+            fiftyFC_Button,hundredFC_Button, clear_Button, unready_Button;
+
+    // All the Labels
+    @FXML
+    private Label enterAmount_Label, total_Label, ccStored_Label, message_Label, change_Label, tabName_Label, date_Label;
+
+    // All the Textfields
+    @FXML
+    private TextField amount_TF, ccNum_TF, expDate_TF;
+
+    private Node[] disable_enable_paymentNodes;
     @FXML
     public void initialize() {
 
@@ -80,9 +95,80 @@ public class UserTransactionController {
             button.setVisible(true);
             button.setText(productName);
         }
+
+        // Create node array for disabline and enabling easier
+        disable_enable_paymentNodes = new Node[]{storedcc_Button, newcc_Button, clear_Button, tender_Button,
+                exactCash_Button, tenFC_Button, twentyFC_Button, fiftyFC_Button, hundredFC_Button, amount_TF, ccNum_TF, expDate_TF};
+        disableNotReady();
     }
 
+    // Done button
     public void doneButtonPushed(ActionEvent event) throws IOException {
+        // TODO we need to disable and clear
+        clearTF();
+        disableNotReady();
         SceneManager.getInstance().changeParent("Tab");
+    }
+    // Ready Button Pushed
+    public void readyButtonPushed(ActionEvent event) throws IOException{
+        enableReady();
+    }
+
+    // Reoder Button Pushed
+    public void reorderButtonPushed(ActionEvent event) throws IOException{
+        // TODO When an item is selected in the lisview duplicate it
+    }
+
+    // void Button Pushed
+    public void voidButtonPushed(ActionEvent event) throws IOException{
+        // TODO When an item is selected delete it from the listview
+    }
+
+    // stored CC Pushed
+    public void runStoredCCPushed(ActionEvent event) throws IOException{
+        // TODO really this will just disable all other functions since we have it stored followed by a done button press
+    }
+
+    // new CC Pushed
+    public void newCCPushed(ActionEvent event) throws IOException{
+        // TODO this will store the new CC as the payment method. If a stored CC exists, it will replace it in the database
+    }
+
+    // any fast cash button can be pressed
+    public void fastcashButtonPressed(ActionEvent event) throws IOException{
+        // TODO this will read the text field from the exact cash button pressed and set the change label accordingly
+    }
+
+    // Tender button pushed
+    public void tenderPressed(ActionEvent event){
+        // TODO this will read the amount text field store it and set the change label accordingly
+    }
+
+    // Clear Button
+    public void clearButtonPressed(ActionEvent event){
+       clearTF();
+    }
+
+    // Undready? Button
+    public void unreadyButtonPressed(ActionEvent event){
+        disableNotReady();
+    }
+
+    // Disable buttons and text fields and set label text
+    private void disableNotReady(){
+        for (Node n : disable_enable_paymentNodes)
+            n.setDisable(true);
+    }
+
+    // Enable Buttons and text fields and label
+    private void enableReady(){
+        for (Node n : disable_enable_paymentNodes)
+            n.setDisable(false);
+    }
+
+    private void clearTF(){
+        amount_TF.clear();
+        ccNum_TF.clear();
+        expDate_TF.clear();
     }
 }
