7da45ea1a64150eb5c56f31a0b58470b5e15f556
John Peel <john@dgby.org>
2019-03-25 12:17:18 -0400

    Reword a comment to better explain what it happening

--- src/main/java/org/dgby/gatorpos/controllers/TabScreenController.java
+++ src/main/java/org/dgby/gatorpos/controllers/TabScreenController.java
@@ -43,7 +43,7 @@ public class TabScreenController {
                 String startVal = newVal.substring(0, 2);
                 String endVal = newVal.substring(newVal.length() - 1);
 
-                // We count the question marks, to test if track 2 is there or not.
+                // We count the end sentinals, to test if track 2 is there or not.
                 long q_count = newVal.chars().filter(ch -> ch == '?').count();
 
                 // q_count must be >= to 2, This means track 2 is also in the input.
@@ -62,7 +62,7 @@ public class TabScreenController {
 
                     if (startVal.equals("%E")) {
                         Platform.runLater(() -> {
-                            // TODO: Make this an alert!
+                            // NOTE: Make this an alert?
                             name_TF.setText("Swipe Error, Try Again!");
                             name_TF.selectAll();
                         });
